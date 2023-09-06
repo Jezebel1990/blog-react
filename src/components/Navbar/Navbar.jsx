4fc846120824d366dc115b61e1e2d3a08851c7b3
Jezebel1990 <jezebelguedes@gmail.com>
2023-09-06 12:15:12 -0300

    feat: add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,51 @@
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+import { RiNewspaperFill } from "react-icons/ri";
+import { FaBars } from "react-icons/fa";
+import search_icon from "../../assets/images/search_icon.png";
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+  <nav className="navbar bg-darkGrey flex align-center">
+ <div className="container w-100">
+    <div className="navbar-content flex align-center justify-between">
+<div className="brand-and-toggler">
+<Link to="/" className="navbar-brand text-white flex align-center fs-26">
+    <span className="navbar-brand-icon">
+        <RiNewspaperFill size = {21}/>
+        <span className="navbar-brand-txt font-rubik fw-5">Blog</span>
+    </span>
+</Link>
+</div>
+
+<div className="navbar-row flex align-center">
+    <ul className="navbar-nav flex align-center font-rubik">
+        <li className="nav-item">
+            <Link to="/" className="nav-link text-white fw-4 fs-19">Home</Link>
+        </li>
+
+        <li className="nav-item">
+            <Link to="/blog" className="nav-link text-white fw-4 fs-19">Blog</Link>
+        </li>
+        <li className="nav-item">
+            <Link to="/about" className="nav-link text-white fw-4 fs-19">Sobre nós</Link>
+        </li>
+    </ul>
+<div className="vertical-line"></div>
+
+<div className="navbar-btns flex align-center">
+    <button type="button" className="navbar-search-btn">
+        <img src= {search_icon} alt="Search icon" />
+    </button>
+<button type="button" className="sidebar-show-btn bg-white flex align-center justify-center">
+ <FaBars size = {21} />
+</button>
+</div>
+</div>
+</div>
+ </div>
+  </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
