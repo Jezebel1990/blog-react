073a8aabff3639745978c9c2f2ecef1f6cb86e46
Jezebel1990 <jezebelguedes@gmail.com>
2023-09-06 20:22:16 -0300

    feat: add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,15 @@
+import React from 'react';
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>
-      
+  <footer className='footer py-4 bg-darkGrey'>
+    <div className='container'>
+        <div className='footer-content text-white text-center'>
+            <p className='copyright-text font-rubik fw-4 ls-1'>Copyright © 2023. Todos os direitos reservados.</p>
+        </div>
     </div>
+  </footer>
   )
 }
 
